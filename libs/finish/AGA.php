6f2f4840964bbc81a214ba8ee62538041e4220bd
KarlaTorres2017 <34536024+KarlaTorres2017@users.noreply.github.com>
2017-12-14 17:35:12 -0600

    Update AGA.php

--- libs/finish/AGA.php
+++ libs/finish/AGA.php
@@ -6,7 +6,7 @@ class AGA{
     
     
 
-private $conclusion="Este es un ejemplo de como deben llenar cada uno su conclusión";
+private $conclusion="Las pruebas de chi cuadrada nos permiten conocer la dependencia entre distintas variables y/o datos de tal manera que nos permita evaluar algunas variables que se encuentren en un estudio y saber si estas tienen relación con algún resultado que pretendamos evaluar empleando hipotesis. Es importante que para lograrlo haya una correcta distrubución y formateo en los datos que se quieran analizar, de lo contrario, el estadístico de prueba no saldría correcto y por ende la prueba no sería efectiva.";
     
 
     
