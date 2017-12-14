b2607d576d3eae386d090e61320ea7df36fa49d5
KarlaTorres2017 <34536024+KarlaTorres2017@users.noreply.github.com>
2017-12-14 17:47:12 -0600

    Update THKC.php

--- libs/finish/THKC.php
+++ libs/finish/THKC.php
@@ -6,7 +6,12 @@ class THKC{
     
     
 
-private $conclusion="";
+private $conclusion="gracias  a este proyecto se vieron diversos temas como fue en el analisis basico de datos, hablando desde
+el punto de la probabilidad y la estadistica, asi como la comprension de los valores como son los cuantitativos y los cualitativos,gracias
+a estos podemos comprender métodos como lo fue chi cuadrada que en terminos simples es la distribucion de la suma de cuadrados de las desviaciones normales 
+estandarizadas,este metodo utilizado como estandar para la demostracion en graficas de los requerimientos solicitados para la comprension de distintas pruebas
+ que se realizaron en relacion a la calidad  de sueño en personas con diferentes caracteristicas.
+ Se conocieron  diferentes librerias bastante utiles para la  facilitacion del codigo,y como se comprendio aun mas la generacion de reportes.";
     
 
     
